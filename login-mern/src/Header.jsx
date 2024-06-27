ce9966c8b9e4f0206d1d56bbdf1cdd31e1cb2a59
Jose Mauro <josemauro.dev@gmail.com>
2024-06-27 16:33:40 -0300

    Change header

--- login-mern/src/Header.jsx
+++ login-mern/src/Header.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import { PersonFill } from 'react-bootstrap-icons';
 import { useNavigate } from "react-router-dom";
 
 function Header(props) {
